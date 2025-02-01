a3bd057905237b1b3b610c85e2923fb0ad3f3b06
HollowCrafter <68031631+Hollow2000@users.noreply.github.com>
2025-02-01 14:46:59 -0600

    pantalla de acceso creada

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { Card } from 'primeng/card';
+import { FloatLabel } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+interface FormLogin {
+  email: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports:  [Card, FloatLabel, PasswordModule, ButtonModule, InputTextModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export default class LoginComponent {
+  private readonly _fb = inject(FormBuilder);
+  private readonly _messageService = inject(MessageService);
+
+  form = this._fb.group<FormLogin>({
+    email: this._fb.control('', [Validators.required, Validators.email]),
+    password: this._fb.control('', [Validators.required])
+  });
+
+  submit() {
+    this.form.markAllAsTouched();
+    if (this.form.valid) {
+      
+
+    } else {
+      this._messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: 'Por favor, revisa los campos marcados',
+        life: 3000
+      });
+    }
+  }
 
 }
