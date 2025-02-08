b4ef6713f0e8055d500ff4c5237750d87061ef7c
HollowCrafter <68031631+Hollow2000@users.noreply.github.com>
2025-02-07 20:16:43 -0600

    Renombrado home -> movements

--- src/app/pages/main/main.routes.ts
+++ src/app/pages/main/main.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from "@angular/router";
 
 export const mainRoutes: Routes = [
     {
-        path: '', loadComponent: () => import('./home/home.component')
+        path: '', loadComponent: () => import('./movements/movements.component').then(c => c.MovementsComponent)
     },
     {
         path: 'ahorros', loadComponent: () => import('./savings/savings.component').then(c => c.SavingsComponent)
--- src/app/pages/main/movements/movements.component.ts
+++ src/app/pages/main/movements/movements.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  templateUrl: './movements.component.html',
+  styleUrl: './movements.component.css'
 })
-export default class HomeComponent {
+export class MovementsComponent {
  
 }
