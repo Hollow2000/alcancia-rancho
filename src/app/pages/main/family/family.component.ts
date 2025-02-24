757a99c4ee9c063c68b19da32b6998556bb784c3
Saul Moedano <smoedanom@inbursa.com>
2025-02-24 11:38:39 -0600

    Filtrado de familiares y arreglos esteticos para la pantalla de famiiares y ahorros

--- src/app/pages/main/family/family.component.ts
+++ src/app/pages/main/family/family.component.ts
@@ -1,16 +1,20 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
 import { Button } from 'primeng/button';
 import { Card } from 'primeng/card';
 import { DataView } from 'primeng/dataview';
 import { AvatarModule } from 'primeng/avatar';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 import { Subscription } from 'rxjs';
 import { DeviceService } from '../../../services/device.service';
 import { Family, FamilyService } from '../../../services/family.service';
+import { InputText } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-family',
   standalone: true,
-  imports: [Card, Button, DataView, AvatarModule],
+  imports: [Card, Button, DataView, AvatarModule, IconFieldModule, InputIconModule, InputText, FormsModule],
   templateUrl: './family.component.html',
   styleUrl: './family.component.css'
 })
@@ -19,6 +23,7 @@ export class FamilyComponent implements OnInit, OnDestroy {
   private readonly _familyService = inject(FamilyService);
   
   family$ = this._familyService.getFamilyList();
+  filter = '';
 
   private subscription?: Subscription;
   layout: 'list' | 'grid' = 'list';
@@ -39,5 +44,14 @@ export class FamilyComponent implements OnInit, OnDestroy {
 
   getIniciales(pepole: Family){
     return pepole.nombres[0] + pepole.apellidos[0];
+  }
+
+  onFilter(){
+    this.family$ = signal<Family[]>(
+      this._familyService.getFamilyList()().filter(family => 
+        family.nombres.toLowerCase().includes(this.filter.toLowerCase()) || 
+        family.apellidos.toLowerCase().includes(this.filter.toLowerCase())
+      )
+    );
   }
 }
