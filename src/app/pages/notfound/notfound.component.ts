61d792427827b6fcd46e7f8af8f18a60da5fe495
Saul Moedano <smoedanom@inbursa.com>
2025-02-24 16:57:07 -0600

    Not Found page lista

--- src/app/pages/notfound/notfound.component.ts
+++ src/app/pages/notfound/notfound.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { Button } from 'primeng/button';
+import { Card } from 'primeng/card';
 
 @Component({
   selector: 'app-notfound',
   standalone: true,
-  imports: [],
+  imports: [Card, Button],
   templateUrl: './notfound.component.html',
   styleUrl: './notfound.component.css'
 })
 export default class NotfoundComponent {
+  private readonly _router = inject(Router);
 
+  goToHome(){
+    this._router.navigateByUrl('');
+  }
 }
