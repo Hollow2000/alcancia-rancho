5b0d82b480ba5cd36879d412f3bda3282b8ababf
Saul Moedano <smoedanom@inbursa.com>
2025-02-25 12:39:04 -0600

    Auto-generated commit

--- chunk-BIQNUZ7N.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ACKELEN3.js";var o=[{path:"login",loadComponent:()=>import("./chunk-OKZLKGI5.js")},{path:"register",loadComponent:()=>import("./chunk-W25SRCPO.js")}];export{o as authRoutes};
