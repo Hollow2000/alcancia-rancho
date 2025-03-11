1dcf2462a3a7c749294f73c098598e1a27e3cae4
Saul Moedano <smoedanom@inbursa.com>
2025-03-11 16:38:02 -0600

    Auto-generated commit

--- chunk-ENBENPKV.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ACKELEN3.js";var o=[{path:"login",loadComponent:()=>import("./chunk-DQGCTPQU.js")},{path:"register",loadComponent:()=>import("./chunk-HVC4RGTZ.js")}];export{o as authRoutes};
---
+++ chunk-KNKNTG5F.js
@@ -0,0 +1,1 @@
+import"./chunk-ACKELEN3.js";var o=[{path:"login",loadComponent:()=>import("./chunk-DGNKZRUF.js")},{path:"register",loadComponent:()=>import("./chunk-LVKAP3AH.js")}];export{o as authRoutes};
