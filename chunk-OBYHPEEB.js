266b4acd042b97df112248508998fe88ff129162
HollowCrafter <68031631+Hollow2000@users.noreply.github.com>
2025-03-16 22:38:07 -0600

    Auto-generated commit

--- chunk-OBYHPEEB.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-ACKELEN3.js";var o=[{path:"login",loadComponent:()=>import("./chunk-KPJIR2O5.js")},{path:"register",loadComponent:()=>import("./chunk-Q2LSO5AA.js")}];export{o as authRoutes};
